2a4cbe41863ce94356f042d7fd1a551d34c6f453
tomodahinata <hikaru.hinata.hikari@icloud.com>
2022-09-12 15:01:25 +0900

    signup

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,7 +1,27 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"to-do-app/app/models"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	genereateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "Get" {
+		genereateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
 }
